416212f8b791d548f460af8685619452e6367568
Sidney Miranda <sidney.miranda2013@gmail.com>
2026-04-11 20:33:06 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ git pull origin main
 docker-compose up -d --build
 
 # Comandos internos do Laravel
-docker exec corre_app php artisan migrate --force
-docker exec corre_app php artisan optimize
+#docker exec corre_app php artisan migrate --force
+#docker exec corre_app php artisan optimize
 
 echo "✅ Deploy finalizado com sucesso!"
